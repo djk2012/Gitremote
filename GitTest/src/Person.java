42476f28d44428f2dd0d588228ecc022d6cc22a5
JUNKAI DING <dingjunkai8856@gmail.com>
2014-04-03 10:10:03 +0200

    Update Person.java
    final test change

--- GitTest/src/Person.java
+++ GitTest/src/Person.java
@@ -2,7 +2,7 @@ public class Person {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-          System.out.println("AAAAA");
+          System.out.println("LOL");
 	}
 
 }
